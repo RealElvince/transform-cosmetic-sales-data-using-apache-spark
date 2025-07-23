22a563fed0656fc487c27dc525c14d33e74a8292
RealElvince <obuongely@yahoo.com>
2025-07-23 20:13:01 +0300

    data ingestion script

---
+++ scripts/ingest.py
@@ -0,0 +1,17 @@
+import os
+from utils.spark_session import create_spark_session
+from config.schema import schema
+
+def ingest_data(file_path):
+    spark = create_spark_session("DataIngestionApp")
+
+
+    sales_data = spark.read.format("csv") \
+        .option("header", "true") \
+        .schema(schema) \
+        .load(file_path)
+    sales_data.show(10)
+    
+if __name__ == "__main__":
+    file_path = os.path.join("data","sales_data.csv")
+    ingest_data(file_path)
