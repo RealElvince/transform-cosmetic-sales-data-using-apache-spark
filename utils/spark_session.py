3400bce16f9590311cdf8795ce392b0172640482
RealElvince <obuongely@yahoo.com>
2025-07-23 20:11:27 +0300

    code refactor 2

--- utils/spark_session.py
+++ utils/spark_session.py
@@ -2,6 +2,6 @@ from pyspark.sql import SparkSession
 
 
 def create_spark_session(app_name="CosmeticSalesApp"):
-     spark = SparkSession.builder\
+      return SparkSession.builder\
          .appName(app_name)\
          .getOrCreate()
