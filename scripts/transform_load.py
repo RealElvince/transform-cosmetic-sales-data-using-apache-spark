447d2c3d4b61c7a6e81500c30b441c96c237f94e
RealElvince <obuongely@yahoo.com>
2025-07-24 19:17:15 +0300

    transformations and load

---
+++ scripts/transform_load.py
@@ -0,0 +1,36 @@
+from utils.spark_session import create_spark_session
+from scripts.ingest import ingest_data
+import os 
+from pyspark.sql.functions import col,round
+
+
+
+
+
+spark = create_spark_session("CosmeticTransformation")
+
+
+def tranform_sales_data(df):
+
+    # Calculate Revenue Per Box
+
+    revenue_per_box = df.select("Country")\
+    .withColumn(
+        "Revenue_Per_Box",
+        round(col("Amount") / col("Boxes_Shipped"), 2)
+    )\
+    .groupBy("Revenue_Per_Box")\
+    .orderBy("Revenue_Per_Box", ascending=False)
+
+    return revenue_per_box
+
+
+if __name__ == "__main__":
+    file_path = os.path.join(
+        "data","raw","sales_data.csv"
+    )
+    df = ingest_data(file_path)
+    revenue_per_box = tranform_sales_data(df)
+    revenue_per_box.show(5)
+
+    
